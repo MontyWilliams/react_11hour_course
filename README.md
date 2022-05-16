bd1924fbda5489e786061f28c9595577ebff2682
Montywilliams <3650@holbertonschool.com>
2022-05-16 16:59:29 -0600

    this is the minimalist way to get react going, the script is imported in the head tag and ther is no pckg.json

---
+++ README.md
@@ -0,0 +1,7 @@
+### 
+
+
+# To find out which ports are running locally
+```
+sudo netstat -tulpn | grep LISTEN
+```
---
+++ index.js
@@ -0,0 +1,16 @@
+ReactDOM.render(
+<ul>whats up?
+  <li>sky</li>
+  <li>cieling</li>
+  <li>outer space</li>
+</ul>,
+ document.getElementById('root'));
+
+// can also use the queryselector
+// ReactDOM.render(<h1>yo</h1>, document.querySelector("#root"))
+
+// to do this in vanilla js it wold take many mor lines
+// const h1 = document.createElement("h1")
+// h1.textContent = "This is an imperative way to program"
+// h1.className = "header"
+// document.getElementById("root").append(h1)
