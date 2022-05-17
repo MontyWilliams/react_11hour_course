cbecacc85fa1745564cce39a540b2d99af62fd11
Montywilliams <3650@holbertonschool.com>
2022-05-17 03:10:24 -0600

    use only functions to build body of page

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-function Page() {
-    return (
-    <div>
-      <header>
+function Header() {
+  return (
+    <header>
         <nav>
           <img src="./918dcx.png" width="40px" alt="" />
         </nav>
       </header>
-          <h1>Why Im learning react? <br /> why the $$ <br /> Fool</h1>
-          <footer>
-          "© 2023 918dcx development. All rights reserved."
-          </footer>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+  "© 2023 918dcx development. All rights reserved."
+  </footer>
+  )
+}
+
+function Main() {
+  return (
+    <div>
+   <h1>Why Im learning react? <br /> why the $$ <br /> Foolio</h1>
+  </div>
+  )
+}
+function Page() {
+    return (
+    <div>
+      <Header />
+      <Main />
+          <Footer />
     </div>
     )
 }
